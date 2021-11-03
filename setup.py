6e32e9e644f0373d4389e23fa55c3bb716455180
hsorby <h.sorby@auckland.ac.nz>
2021-11-03 13:41:51 +1300

    Use single source version paradigm.

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
+import codecs
 import io
 import os
+import re
 
 from setuptools import setup, find_packages
 
 SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
+def read(*parts):
+    with codecs.open(os.path.join(SETUP_DIR, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 # List all of your Python package dependencies in the
 # requirements.txt file
@@ -26,7 +41,7 @@ source_license = readfile("LICENSE")
 
 setup(
     name='mapclientplugins.imagesourcestep',
-    version='0.1.0',
+    version=find_version('mapclientplugins', 'imagesourcestep', '__init__.py'),
     description='',
     long_description='\n'.join(readme) + source_license,
     long_description_content_type='text/x-rst',
