d9953b3bb4c9110ad2288599bba77e5edc1c3219
Tim Salemink <82302710+tsalemink@users.noreply.github.com>
2025-07-23 22:34:17 +1200

    Convert to implicit namespaces
    The `namespace_packages` parameter is deprecated.

--- setup.py
+++ setup.py
@@ -54,7 +54,6 @@ setup(
     author_email='',
     url='',
     packages=find_packages(exclude=['ez_setup', ]),
-    namespace_packages=['mapclientplugins'],
     include_package_data=True,
     zip_safe=False,
     install_requires=requires,
